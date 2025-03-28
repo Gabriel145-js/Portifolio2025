ef57bce3536d610de990e63642055961fd545041
Gabriel145-js <gabrielzapa456@gmail.com>
2025-03-27 21:40:07 -0300

    feat: melhorar layout da navbar e animações de hover
    - Conversão do Sass para o css e css.map
    - Adicionado efeito de hover com gradiente e transição suave na navbar
    - Ajustado alinhamento dos ícones e textos
    - Implementado deslocamento da navbar para a esquerda ao expandir
    - Melhorada a experiência visual com animações sincronizadas

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/styles.css'
+        })
+    ],
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.sass']
+    }
+};
